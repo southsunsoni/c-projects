9dc8c2f77c7c92da144c6975777f5d5a19d5dedb
southsunsoni <Ivan.soni-tsafack@tu-ilmenau.de>
2023-10-10 14:52:38 +0200

    try

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //a program to convert from one temp to another
+    int radius;
+    cout<<"enter the radius of the circle:";
+    cin>>radius;
+    float area=2*3.14*radius;
+    cout<<area;
+    return 0;
+}
